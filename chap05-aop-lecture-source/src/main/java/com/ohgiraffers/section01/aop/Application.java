5bab15f3b1ad2a39b28d6e7617a837378e8b391e
seongeun223 <jklun080921@gmail.com>
2024-07-08 16:36:56 +0900

    aop reflection

---
+++ chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section01.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.ohgiraffers.section01.aop");
+
+        MemberService memberService = (MemberService) context.getBean("memberService");
+
+        System.out.println("================ selectAllMembers ===============");
+        System.out.println(memberService.selectMembers());
+
+
+        System.out.println("================ selectMemberById ===============");
+        System.out.println(memberService.selectMember(1L));
+    }
+}
